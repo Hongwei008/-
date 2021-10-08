bbd01a8eace288f7b1dab39a9330b580fb59db58
Hongwei008 <48981831+Hongwei008@users.noreply.github.com>
2021-10-08 10:25:10 +0800

    Update stockOperationAnalysis.py

--- stockOperationAnalysis.py
+++ stockOperationAnalysis.py
@@ -5,70 +5,70 @@ import time
 import sys
 import os
 
-def getName(record,history_str):
-	name= (history_str[record[1]])[0][21:]
+def getName(record,history_str1):
+	name= (history_str1[record[1]])[0][21:]
 
 	return name
 
 		
-def getPrice(record,history_str):
+def getPrice(record,history_str1):
 	
-	now= (history_str[record[1]])[3]
+	now= (history_str1[record[1]])[3]
 
 	return now[:-1]
 	
-def getRate(record,history_str):
+def getRate(record,history_str1):
 	
-	buy5=float(0.3*int((history_str[record[1]])[10])+0.3*int((history_str[record[1]])[12])+0.2*int((history_str[record[1]])[14])+0.1*int((history_str[record[1]])[16])+0.1*int((history_str[record[1]])[18]))
-	sell5=float(0.3*int((history_str[record[1]])[20])+0.3*int((history_str[record[1]])[22])+0.2*int((history_str[record[1]])[24])+0.1*int((history_str[record[1]])[26])+0.1*int((history_str[record[1]])[28]))
+	buy5=float(0.3*int((history_str1[record[1]])[10])+0.3*int((history_str1[record[1]])[12])+0.2*int((history_str1[record[1]])[14])+0.1*int((history_str1[record[1]])[16])+0.1*int((history_str1[record[1]])[18]))
+	sell5=float(0.3*int((history_str1[record[1]])[20])+0.3*int((history_str1[record[1]])[22])+0.2*int((history_str1[record[1]])[24])+0.1*int((history_str1[record[1]])[26])+0.1*int((history_str1[record[1]])[28]))
 	if buy5>sell5:
-		rate= str(format(float((history_str[record[1]])[3])/float((history_str[record[1]])[2])-1,'.2%'))+'↑'*int(buy5/sell5)
+		rate= str(format(float((history_str1[record[1]])[3])/float((history_str1[record[1]])[2])-1,'.2%'))+'↑'*int(buy5/sell5)
 	else:
-		rate= str(format(float((history_str[record[1]])[3])/float((history_str[record[1]])[2])-1,'.2%'))+'↓'*int(sell5/buy5)
+		rate= str(format(float((history_str1[record[1]])[3])/float((history_str1[record[1]])[2])-1,'.2%'))+'↓'*int(sell5/buy5)
 	
 	return rate
 
 		
-def buyingAnalysis(record,history_str):
+def buyingAnalysis(record,history_str1):
 	code=record[1]
 	price=record[2]
 	volume=record[3]
-	str0=getName(record,history_str)+" "+getPrice(record,history_str)+" "+getRate(record,history_str)+" 买入收益：{:.2f}".format(volume*(float(getPrice(record,history_str))-price))+" | "
-	profit=volume*(float(getPrice(record,history_str))-price)
+	str0=getName(record,history_str1)+" "+getPrice(record,history_str1)+" "+getRate(record,history_str1)+" 买入收益：{:.2f}".format(volume*(float(getPrice(record,history_str1))-price))+" | "
+	profit=volume*(float(getPrice(record,history_str1))-price)
 	return str0,profit
 
-def selling_analysis(record,history_str):
+def selling_analysis(record,history_str1):
 	code=record[1]
 	price=record[2]
 	volume=record[3]
-	str0=getName(record,history_str)+" "+getPrice(record,history_str)+" "+getRate(record,history_str)+" 卖出收益：{:.2f}".format(volume*(-float(getPrice(record,history_str))+price))+" | "
-	profit=volume*(-float(getPrice(record,history_str))+price)
+	str0=getName(record,history_str1)+" "+getPrice(record,history_str1)+" "+getRate(record,history_str1)+" 卖出收益：{:.2f}".format(volume*(-float(getPrice(record,history_str1))+price))+" | "
+	profit=volume*(-float(getPrice(record,history_str1))+price)
 	return str0,profit
 
-def historyAnalysis(records,history_str):
+def historyAnalysis(records,history_str1):
 	profit=0
 	for record in records:
 		
 		if record[0]=='买入':
-			text0,profit2=buyingAnalysis(record,history_str)
+			text0,profit2=buyingAnalysis(record,history_str1)
 		else:
-			text0,profit2=selling_analysis(record,history_str)
+			text0,profit2=selling_analysis(record,history_str1)
 		profit=profit+profit2
 	str0="，历史收益：{:.2f}".format(profit)
 
 	return str0,profit
 
-def recentAnalysis(records,history_str):
+def recentAnalysis(records,history_str1):
 	text=""
 	profit=0
 	profit0=0
 	profit1=0
 	for record in records:
 		if record[0]=='买入':
-			text0,profit2=buyingAnalysis(record,history_str)
+			text0,profit2=buyingAnalysis(record,history_str1)
 			profit1=profit1+profit2
 		else:
-			text0,profit2=selling_analysis(record,history_str)
+			text0,profit2=selling_analysis(record,history_str1)
 			profit0=profit0+profit2
 		text=text0+text
 		profit=profit1+profit0
@@ -85,23 +85,25 @@ def getData(records):
 	return dic
 
 
-history_records=[['卖出','sz300498',14.71,16600],['卖出','sz300498',14.8,1500],['买入','sz300498',14.324,18100],['卖出','sz300766',13.01,20000],['卖出','sz300766',14.68,20000],['买入','sz300766',14.88,19700],['卖出','sz300766',14.51,19700],['买入','sz300766',14.36,19800],['卖出','sz300766',14.40,19800],['买入','sz300766',13.94,19800],['卖出','sz300987',34,8300],['买入','sz300766',13.02,20000],['买入','sz300987',31.688,8300],['卖出','sz300766',13.3,20000]]
-recent_operation=[['买入','sz300498',14.49,10100],['买入','sz300002',5.8,20600]]
+history_records=[['买入','sz300498',15.16,10100],['卖出','sz300498',15.19,10100],['买入','sz300498',14.49,10100],['买入','sz300002',5.8,20600],['卖出','sz300498',14.71,16600],['卖出','sz300498',14.8,1500],['买入','sz300498',14.324,18100],['卖出','sz300766',13.01,20000],['卖出','sz300766',14.68,20000],['买入','sz300766',14.88,19700],['卖出','sz300766',14.51,19700],['买入','sz300766',14.36,19800],['卖出','sz300766',14.40,19800],['买入','sz300766',13.94,19800],['卖出','sz300987',34,8300],['买入','sz300766',13.02,20000],['买入','sz300987',31.688,8300],['卖出','sz300766',13.3,20000]]
+recent_operation=[['买入','sz300498',14.88,8300],['卖出','sz300002',6,20600]]
 input("任意键开始")
 os.system("cls")
 
 global history_str
 global recent_str
 
-history_str=getData(history_records)
-recent_str=getData(recent_operation)
 
-history_text,history_profit=historyAnalysis(history_records,history_str)
+
+
+
 
 msg0=""
 while True:
 	try:
-		
+		history_str=getData(history_records)
+		recent_str=getData(recent_operation)
+		history_text,history_profit=historyAnalysis(history_records,history_str)
 		recent_text,recent_profit=recentAnalysis(recent_operation,recent_str)
 		
 		text=recent_text+history_text+"，操作总收益：{:.2f}".format(history_profit+recent_profit)
